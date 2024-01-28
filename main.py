038a4b0c7a47803f4a6f3963c7d28912a740d577
kaveheng <58515519+kaveheng@users.noreply.github.com>
2024-01-28 12:35:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,42 +3,41 @@ from indeterminatebeam import Beam, Support, PointLoadV, DistributedLoadV
 
 app = Flask(__name__)
 
-# Global variable to store the beam instance
-beam_instance = None
+# Initialize an empty beam object
+beam = None
 
 @app.route('/setup_beam', methods=['POST'])
 def setup_beam():
-    global beam_instance
+    global beam
     data = request.get_json()
-
-    # Extract beam properties and loads from the request
     length = data['length']
     E = data['E']
     I = data['I']
-    supports = data['supports']
-    loads = data['loads']
 
-    # Initialize the beam
-    beam_instance = Beam(length, E, I)
+    # Create the beam instance
+    beam = Beam(length, E, I)
 
     # Add supports
-    for support in supports:
-        beam_instance.add_support(Support(coord=support['coord'], fixed=support['fixed']))
+    for support in data['supports']:
+        beam.add_supports(Support(support['coord'], tuple(support['fixed'])))
 
     # Add loads
-    for load in loads:
+    for load in data['loads']:
         if load['type'] == 'point':
-            beam_instance.add_load(PointLoadV(load['magnitude'], load['location']))
+            beam.add_loads(PointLoadV(load['magnitude'], load['location']))
         elif load['type'] == 'distributed':
-            beam_instance.add_load(DistributedLoadV(load['magnitude'], load['range']))
+            beam.add_loads(DistributedLoadV(load['magnitude'], tuple(load['range'])))
 
-    return jsonify({'message': 'Beam setup successful'})
+    # Perform analysis
+    beam.analyse()
+
+    return jsonify({'message': 'Beam setup and analysis successful'})
 
 @app.route('/get_shear_force', methods=['POST'])
 def get_shear_force():
     data = request.get_json()
     x_positions = data['x_positions']
-    shear_forces = [beam_instance.get_shear_force(x) for x in x_positions]
+    shear_forces = [beam.get_shear_force(x) for x in x_positions]
 
     return jsonify({'shear_forces': shear_forces})
 
@@ -46,7 +45,7 @@ def get_shear_force():
 def get_bending_moment():
     data = request.get_json()
     x_positions = data['x_positions']
-    bending_moments = [beam_instance.get_bending_moment(x) for x in x_positions]
+    bending_moments = [beam.get_bending_moment(x) for x in x_positions]
 
     return jsonify({'bending_moments': bending_moments})
 
